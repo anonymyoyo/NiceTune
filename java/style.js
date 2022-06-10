3e94f4fc5a70f7bf8dbd64963a51aaf117a1458e
anonymeyoyo <paulemmanuelbohole5@gmail.com>
2022-06-10 19:03:59 +0200

    playlist page

--- java/style.js
+++ java/style.js
@@ -3,7 +3,15 @@ var menu = document.querySelector('.menu');
 
 // creation de l'evenement de click sous-forme d'une fonction
 
-logo.addEventListener('click', function()
-{
-    menu.classList.toggle('showmenu')
-});
+// logo.addEventListener('click', function()
+// {
+//     menu.classList.toggle('showmenu')
+// });
+
+// var MaMusique = document.getElementById("#MaMusique");
+// var btnplay = document.getElementById("#btnplay");
+
+// btnplay.onclick = function()
+// {
+//     MaMusique.onplay();
+// }
